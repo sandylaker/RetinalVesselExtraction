32b97c32b2381d2f1466cc8dd98aec57d09c18bc
sandylaker <lfc199471@gmail.com>
2019-06-12 11:51:39 +0200

    add canny_weight map and update training function

--- utils/loss.py
+++ utils/loss.py
@@ -5,23 +5,17 @@ import numpy as np
 
 class BCEWithLogitsLoss2d(nn.Module):
 
-    def __init__(self, weight=None, reduction='mean', pos_weight=None):
+    def __init__(self, reduction='mean', pos_weight=None):
         """
         this class computes the binary cross entropy loss for a 2d image
 
-        :param weight: (Tensor, optional) - 2d weight maps, has to be a Tensor of size n-batch.
         :param reduction: see docs of nn.BCEWithLogitsLoss on pytorch website
         :param pos_weight: see docs of nn.BCEWithLogitsLoss on pytorch website
         """
         super(BCEWithLogitsLoss2d, self).__init__()
-        # TODO substitute weight for each batch with weight_map for each input image
-        if weight is None:
-            self.weight = None
-        else:
-            self.weight = weight.view(-1)
-        self.loss = nn.BCEWithLogitsLoss(self.weight, reduction=reduction, pos_weight=pos_weight)
+        self.loss = nn.BCEWithLogitsLoss(reduction=reduction, pos_weight=pos_weight)
 
-    def forward(self, logits, targets):
+    def forward(self, logits, targets, weights=None):
         """
         forward function
 
@@ -29,6 +23,7 @@ class BCEWithLogitsLoss2d(nn.Module):
         :param targets: (Tensor) - the ground truth label of each pixel
         :return: scalar. If reduction is 'none', then (N,*), same as logits input.
         """
+        # TODO add weight for each classification map
         logits = logits.contiguous().view(-1)
         targets = targets.contiguous().view(-1)
         return self.loss(logits, targets)
@@ -36,19 +31,18 @@ class BCEWithLogitsLoss2d(nn.Module):
 
 class SoftDiceLoss(nn.Module):
 
-    def __init__(self, weight=None):
+    def __init__(self):
         super(SoftDiceLoss, self).__init__()
-        self.weight = weight
 
-    def forward(self, logits, targets):
+    def forward(self, logits, targets, weights=None):
         proba = torch.sigmoid(logits)
         num = targets.size(0)
-        if not self.weight:
-            weight = torch.ones_like(targets)
+        if weights is None:
+            weights = torch.ones_like(targets)
         else:
-            weight = self.weight
-        targets = weight * targets
-        proba = weight * proba
+            weights = weights
+        targets = weights * targets
+        proba = weights * proba
         proba = proba.view(num, -1)
         targets = targets.view(num, -1)
 
